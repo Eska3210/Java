a8c024677a5fe1a96cc8b048c0f11272f5cdea62
Eska3210 <167294558+Eska3210@users.noreply.github.com>
2024-11-15 13:53:17 +0000

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello;
+
+/**
+ *
+ * @author EskaRashid
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello everyone, its Eska,  We are going to learn Java!";        
+        
+        
+    }
+}
